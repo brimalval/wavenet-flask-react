205e3cecf18fe7c2b213b9c4c4239f00a63cc853
Brian Valencia <qbmsvalencia@tip.edu.ph>
2022-04-08 02:15:40 +0800

    Added key & tempo form with basic validation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,23 +1,43 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
+import { Key } from "../utils/types/Key";
+import * as Yup from "yup";
+
+type Request = {
+  key: Key;
+  tempo: number;
+};
 
 function Home() {
+  const initialValues: Request = {
+    key: Key.C,
+    tempo: 120,
+  };
+  const validationSchema = Yup.object({
+    key: Yup.string().required("Key is required"),
+    tempo: Yup.number().required("Tempo is required").min(45).max(280),
+  });
   return (
     <Formik
-      initialValues={{ name: "" }}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
       onSubmit={(values, { setSubmitting }) => {
         console.log(values);
         setSubmitting(false);
       }}
     >
-      {({ values, handleChange, handleBlur, isSubmitting }) => (
+      {({ isSubmitting, errors }) => (
         <Form>
-          <Field
-            name="name"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.name}
-          />
-          <ErrorMessage name="name" component="div" />
+          <Field name="key" as="select">
+            {Object.values(Key).map((val) => (
+              <option key={val} value={val}>
+                {val}
+              </option>
+            ))}
+          </Field>
+
+          <Field name="tempo" />
+          {errors.tempo && <div className="text-red-500">{errors.tempo}</div>}
+
           <button type="submit" disabled={isSubmitting}>
             Submit
           </button>
---
+++ frontend/src/utils/types/Key.ts
@@ -0,0 +1,13 @@
+export enum Key {
+  Asharp = "A#",
+  B = "B",
+  C = "C",
+  Csharp = "C#",
+  D = "D",
+  Dsharp = "D#",
+  E = "E",
+  F = "F",
+  Fsharp = "F#",
+  G = "G",
+  Gsharp = "G#",
+}
