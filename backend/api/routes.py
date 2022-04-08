513d5674d2f49c87c9d1b47cf13e24792a519149
Brian Valencia <qbmsvalencia@tip.edu.ph>
2022-04-09 02:57:50 +0800

    Added arbitrary test for bad requests

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from flask_api import status
 
 api = Blueprint('api', __name__)
 
@@ -16,4 +17,9 @@ def name(name):
 
 @api.route("/post", methods=["POST"])
 def post():
+	if int(request.get_json()["tempo"]) == 123:
+		resp = request.get_json()
+		resp["message"] = "Arbitrary bad request number"
+		return jsonify(resp), status.HTTP_400_BAD_REQUEST
+
 	return jsonify(request.get_json())
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,8 @@
 import ParamsRequest from "./types/ParamsRequest";
 import axios from "axios";
 export async function postParams(request: ParamsRequest) {
-  const response = await axios.post("/api/post", request);
-  return response.data;
+  const response = await axios.post("/api/post", request).catch((error) => {
+    return error.response;
+  });
+  return response;
 }
