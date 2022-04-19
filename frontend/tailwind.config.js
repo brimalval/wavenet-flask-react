d7fd449fb3ffaec38f084198d9f8ca2fd21bbca0
qbmsvalencia <qbmsvalencia@tip.edu.ph>
2022-04-20 02:53:49 +0800

    Fix Tailwind breakpoints to match Material-UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "0px",
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
     extend: {
       colors: {
         primary: "#577B7C",
