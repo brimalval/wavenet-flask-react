869e08a30e72a78c5159e1c8a5aa4c278bc7af15
qbmsvalencia <qbmsvalencia@tip.edu.ph>
2022-04-20 02:53:28 +0800

    Add theme to apply default font

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,6 @@
+import { createTheme, ThemeProvider } from "@mui/material";
+import createPalette from "@mui/material/styles/createPalette";
+import createTypography from "@mui/material/styles/createTypography";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Dashboard from "./pages/Dashboard";
@@ -5,16 +8,23 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const theme = createTheme({
+    typography: createTypography(createPalette({}), {
+      fontFamily: "Open Sans, sans-serif",
+    })
+  })
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/dashboard" element={<MainLayout />}>
-          <Route index element={<Dashboard />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/dashboard" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
