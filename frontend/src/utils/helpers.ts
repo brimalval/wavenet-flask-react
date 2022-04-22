958f98e02514a6aa1146db16185792346ec1bb96
qbmsvalencia <qbmsvalencia@tip.edu.ph>
2022-04-23 02:17:55 +0800

    Refactor helper function for playing MIDI

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -11,25 +11,30 @@ export const downloadBlob = (blob: Blob, fileName?: string) => {
   link.remove();
 };
 
-export const playBlob = async (blob: Blob, instrument: Soundfont.InstrumentName) => {
+export const setupPlayer = async (instrument: Soundfont.InstrumentName) => {
   // Set up the player
   var soundfontPlayer = await Soundfont.instrument(new AudioContext(), instrument);
-  const Player = new MidiPlayer.Player(function (event: any) {
+  const midiPlayer = new MidiPlayer.Player(function (event: any) {
     if (event.name === "Note on") {
       soundfontPlayer.play(event.noteName);
     } else if (event.name === "Note off") {
       soundfontPlayer.stop();
     }
   });
+  return midiPlayer;
+}
+
+export const playBlob = async (blob: Blob, instrument: Soundfont.InstrumentName, player?: MidiPlayer.Player) => {
+   player ??= await setupPlayer(instrument);
   // Create Base64 encoding of the blob
   var reader = new FileReader();
   reader.readAsDataURL(blob);
   reader.onloadend = function () {
     var base64data = reader.result;
     const url = base64data?.toString();
-    if (url) {
-      Player.loadDataUri(url);
-      Player.play();
+    if (url && player) {
+      player.loadDataUri(url);
+      player.play();
     }
   };
 };
