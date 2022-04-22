6274490624c510d69226293bb1cd84a6057bf069
qbmsvalencia <qbmsvalencia@tip.edu.ph>
2022-04-22 19:38:04 +0800

    Update model prediction return to show octave

--- backend/model/model.py
+++ backend/model/model.py
@@ -48,4 +48,4 @@ class Model:
             x_data = np.array(x).reshape(1, sequence_length, 1).astype("float32")
         # TODO: save created midi file
         self.converter.create_song_from_ints(output, file_name)
-        return [self.converter.map_int_to_note(int_note).name for int_note in output]
+        return [self.converter.map_int_to_note(int_note).nameWithOctave for int_note in output]
